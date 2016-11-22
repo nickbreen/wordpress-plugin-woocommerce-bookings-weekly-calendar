e86d60c8afad147d65351e6bf33c60fa1805a150
Nick Breen <nick@foobar.net.nz>
2016-11-22 22:18:27 +1300

    refactor to use only the pods driver type

--- assets/js/drivers.js
+++ assets/js/drivers.js
@@ -1,10 +1,5 @@
 jQuery(function($) {
 
-    var me = null;
-    var drivers = [];
-    var driverBookings = [];
-    var bookings = $('li.booking');
-
     $('table.wc_bookings_calendar_weekly td.booked').droppable({
         accept: 'ul.drivers li',
         classes: {
@@ -12,11 +7,12 @@ jQuery(function($) {
         },
         hoverClass: "ui-state-hover", // pre 1.12.0
         drop: function (event, ui) {
+            console.log(this)
             var driver = ui.draggable.get(0).dataset.driverId
-            var bookings = $.map($(this).find('li').get(), function (e, i) {
+            var bookings = $.map($(this).find('li.booking').get(), function (e, i) {
                 return e.dataset.bookingId;
             })
-            $.ajax(wpApiSettings.root + 'wp/v2/driver', {
+            $.ajax(wpApiSettings.root + 'wp/v2/driver', $.extend({}, ajaxDefaults, {
                 method: 'POST',
                 contentType: 'application/json; charset=UTF-8',
                 dataType: 'json',
@@ -24,71 +20,46 @@ jQuery(function($) {
                     user: driver,
                     bookings: bookings
                 },
-            })
+            }))
         },
     })
 
-    $.ajaxSetup({
+    var ajaxDefaults = {
         headers: {
             'X-WP-Nonce': wpApiSettings.nonce
         },
         error: function (jqXHR, textStatus, errorThrown) {
             console.error("%s; %s", textStatus, errorThrown);
         },
-        complete: function (jqXHR, textStatus) {
-            console.dir({driverBookings:driverBookings, drivers:drivers, bookings:bookings});
+    }
 
-            for (i in driverBookings) {
-                bookings.filter(function (j, e) {
-                    // console.log(parseInt(this.dataset.bookingId), driverBookings[i].bookings, driverBookings[i].bookings.indexOf(parseInt(this.dataset.bookingId)))
-                    return -1 != driverBookings[i].bookings.indexOf(parseInt(this.dataset.bookingId))
-                })
-                .closest('td')
-                .filter(function () {
-                    return driverBookings[i].user in drivers
-                })
-                .css('background-color', drivers[driverBookings[i].user]['background-color'])
-            }
-        },
-    });
-
-    $.ajax(wpApiSettings.root + 'wp/v2/users', {
-        data: {
-            roles: 'driver',
-            context: 'edit',
-            orderby: 'name'
-        },
+    $.ajax(wpApiSettings.root + 'wp/v2/driver', $.extend({}, ajaxDefaults, {
         success: [
             function (data, textStatus, jqXHR) {
-                for (i in data) {
-                    var r = 360*5*i/12;
-                    data[i]['background-color'] = 'hsla('+r+', 50%, 50%, 0.25)';
-                }
-            },
-            function (data, textStatus, jqXHR) {
-                for (i in data)
-                    drivers[data[i].id] = data[i]
-            },
-            function (data, textStatus, jqXHR) {
                 $('<tfoot><tr><th>Drivers<td colspan="7"><ul class="drivers">').appendTo('table.wc_bookings_calendar_weekly')
-
-                $('<li>').appendTo('ul.drivers').text("no driver").after(
-                    $.map(data, function (element, i) {
-                        return $('<li>')
-                            .text(element.name)
-                            .data(element)
-                            .attr('data-driver-id', element.id)
-                            .css('background-color', element['background-color'])
+                $('<li>').text("no driver")
+                    .appendTo('ul.drivers').after(
+                        $.map(data, function (element, i) {
+                            return $('<li>').addClass('driver')
+                                .text(element.title.rendered)
+                                .data(element)
+                                .css('background-color', element.colour)
+                        })
+                    )
+                    .siblings().addBack().draggable({helper: 'clone', opacity: 0.7})
+            },
+            function (data, textStatus, jqXHR) {
+                data.filter(function (e, i) {
+                    return e.bookings
+                }).map(function (e, i) {
+                    $('td.booked').filter(function (j, f) {
+                        return $('li.booking', this).filter(function (k, g) {
+                            return e.bookings.includes(parseInt(this.dataset.bookingId))
+                        }).length
                     })
-                ).siblings('li').addBack().draggable({helper: 'clone', opacity: 0.7})
-            }
+                    .css('background-color', e.colour)
+                })
+            },
         ]
-    });
-
-    $.ajax(wpApiSettings.root + 'wp/v2/driver', {
-        success: function (data, textStatus, jqXHR) {
-            driverBookings = data;
-        }
-    })
-
+    }))
 });
