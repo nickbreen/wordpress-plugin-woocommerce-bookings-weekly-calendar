f2a19707d2ed2a3948ea81e01f425aec80be37f5
Nick Breen <nick@foobar.net.nz>
2016-11-23 00:02:12 +1300

    fixed POST-ing/PATCH-ing the bookings

--- assets/js/drivers.js
+++ assets/js/drivers.js
@@ -1,29 +1,5 @@
 jQuery(function($) {
 
-    $('table.wc_bookings_calendar_weekly td.booked').droppable({
-        accept: 'ul.drivers li',
-        classes: {
-            "ui-droppable-hover": "ui-state-hover"
-        },
-        hoverClass: "ui-state-hover", // pre 1.12.0
-        drop: function (event, ui) {
-            console.log(this)
-            var driver = ui.draggable.get(0).dataset.driverId
-            var bookings = $.map($(this).find('li.booking').get(), function (e, i) {
-                return e.dataset.bookingId;
-            })
-            $.ajax(wpApiSettings.root + 'wp/v2/driver', $.extend({}, ajaxDefaults, {
-                method: 'POST',
-                contentType: 'application/json; charset=UTF-8',
-                dataType: 'json',
-                data: {
-                    user: driver,
-                    bookings: bookings
-                },
-            }))
-        },
-    })
-
     var ajaxDefaults = {
         headers: {
             'X-WP-Nonce': wpApiSettings.nonce
@@ -31,7 +7,43 @@ jQuery(function($) {
         error: function (jqXHR, textStatus, errorThrown) {
             console.error("%s; %s", textStatus, errorThrown);
         },
+        beforeSend: function (jqXHR, settings) {
+            console.dir(settings.data)
+        },
+        complete: function (jqXHR, textStatus) {
+            console.info(textStatus);
+        },
+        success: function (data, textStatus, jqXHR) {
+            console.dir(data);
+        },
     }
+
+    $('table.wc_bookings_calendar_weekly td.booked').droppable({
+        accept: 'ul.drivers li',
+        classes: {
+            "ui-droppable-hover": "ui-state-hover"
+        },
+        hoverClass: "ui-state-hover", // pre 1.12.0
+        drop: function (event, ui) {
+            if (!ui.draggable.data('driver')) return
+            var url = wpApiSettings.root + 'wp/v2/driver/' + ui.draggable.data('driver').id
+            var bookings = $.merge(
+                $.isArray(ui.draggable.data('driver').bookings) ? ui.draggable.data('driver').bookings : [],
+                $(this).find('li.booking').map(function (i, e) {
+                    return parseInt(this.dataset.bookingId);
+                }).get()
+            )
+            $.ajax(url, $.extend({}, ajaxDefaults, {
+                method: 'PATCH',
+                contentType: 'application/json; charset=UTF-8',
+                processData: false,
+                data: JSON.stringify({
+                    status: 'private',
+                    'bookings': bookings
+                }),
+            }))
+        },
+    })
 
     $.ajax(wpApiSettings.root + 'wp/v2/driver', $.extend({}, ajaxDefaults, {
         success: [
@@ -42,7 +54,7 @@ jQuery(function($) {
                         $.map(data, function (e, i) {
                             return $('<li>').addClass('driver')
                                 .text(e.title.rendered)
-                                .data(e)
+                                .data('driver', e)
                                 .css('background-color', $.Color(e.colour).lightness(0.75))
                         })
                     )
